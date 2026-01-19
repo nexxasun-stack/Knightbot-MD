7a80b6c1dd62ca3ccfe5dd84efc4c7ce0dd1a1fe
nexxasun-stack <nexxasun@gmail.com>
2026-01-19 19:45:17 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight Bot',
+  packname: 'Bhavy Bot',
   author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botName: "Bhavy Bot",
+  botOwner: 'Bhavy', // Your name
+  ownerNumber: '23274230770', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
